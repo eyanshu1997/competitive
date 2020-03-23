192b7e8844ad08781750b606187c630119ed23b0
aurav <eyanshu1997@gmail.com>
2020-03-23 21:57:27 +0530

    linked list complete

--- linkedlist.cc
+++ linkedlist.cc
@@ -111,6 +111,52 @@ int getlength(Node *head)
 	}
 	return count;
 }
+void swap(Node **head,int x, int y)
+{
+	if(x==y)
+	return;
+	Node *prevX = NULL, *currX = *head;  
+	while (currX && currX->data != x)  
+	{  
+	    prevX = currX;  
+	    currX = currX->next;  
+	}  
+	Node *prevY = NULL, *currY = *head;  
+	while (currY && currY->data != y)  
+	{  
+	    prevY = currY;  
+	    currY = currY->next;  
+	}  
+	  
+	if (currX == NULL || currY == NULL)  
+	    return;  
+	if (prevX != NULL)  
+	    prevX->next = currY;  
+	else
+	    *head = currY;  
+
+	if (prevY != NULL)  
+	    prevY->next = currX;  
+	else 
+	    *head = currX;  
+ 
+	Node *temp = currY->next;  
+	currY->next = currX->next;  
+	currX->next = temp;  
+}
+void reverse(Node **head) 
+{ 
+	Node* current = *head; 
+	Node *prev = NULL, *next = NULL;
+	while (current != NULL) 
+	{ 
+		next = current->next; 
+		current->next = prev;
+		prev = current; 
+		current = next; 
+	} 
+	(*head) = prev; 
+} 
 int main()
 {
 	Node *head=NULL;
@@ -122,7 +168,9 @@ int main()
 	printlist(head);
 	//deletenode(&head,10);
 	//deletenode(&head,5);
-	deleteposition(&head,3);
+	//deleteposition(&head,3);
+//	swap(&head,10,5);
+	reverse(&head);
 	printlist(head);
 	cout<<"length is : "<<getlength(head)<<"\n";
 	return 0;
